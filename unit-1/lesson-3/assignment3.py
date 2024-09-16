1d00abd6a92707b1a32833e4777be007d68c77c0
Zadazou <zouw819@newschool.edu>
2024-09-16 15:45:59 -0400

    assignment3 complete

--- unit-1/lesson-3/assignment3.py
+++ unit-1/lesson-3/assignment3.py
@@ -1,7 +1,9 @@
 import sys
-word_list = [ "astonishing", "rehabilitation", "understanding", "mathematics", "liability", "obligation", "fashionable", "organisation", "evolution", "excavation", "constitution", "invisible", "implication", "academy", "appreciate", "aquarium", "generation", "reproduction", "accessible", "commemorate", "violation", "integration", "combination", "definition", "emergency", "deprivation", "vegetarian", "photocopy", "radiation", "admiration", "laboratory", "helicopter", "embarrassment", "experience", "hierarchy", "hospitality", "conventional", "deviation", "regulation", "indication", "acquisition", "ambiguous", "civilization", "exaggerate", "intermediate", "recommendation", "casualty", "motivation", "manufacture", "deteriorate" ]
-word_list.sort()
+
 word_list = sys.argv
+
 word_list.pop(0)
-index = int(sys.argv[1])
-print( word_list[index] )
+
+word_list.sort()
+
+print( word_list )
